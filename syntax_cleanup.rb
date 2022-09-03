583db479fadd7458d200fec664eff97397a3a44c
Anhnhi Tran <anhtran811@gmail.com>
2022-09-03 14:11:32 -0600

    Syntax clean up

--- syntax_cleanup.rb
+++ syntax_cleanup.rb
@@ -1,9 +1,20 @@
 # Clean Up the code below to improve readability.
-foods = {"apples": 23, 
-  "grapes": 507, 
+foods = {"apples": 23,
+  "grapes": 507,
   "eggs": 48}
+#clean up:
+foods = { apples: 23, grape: 507, eggs: 48}
+
+#===================================>
 
 person = { "height": "6 ft", "weight": "160 lbs" }
+#clean up:
+person = {
+  height: "6ft",
+  weight: "160lbs"
+}
+
+#===================================>
 
 post = { image_src: "./images/beach.png",
 
@@ -20,4 +31,22 @@ post2 =
       timestamp: "11:37 PM December 31, 2019",
       number_likes: 13,
       comments: []
+}
+
+#clean up:
+post = {
+  image_src: "./images/beach.png",
+  caption: "At the beach with my besties",
+  timestamp: "4:37 PM August 13, 2019",
+  number_likes: 0,
+  comments: []
+}
+
+post2 =
+{
+  image_src: "./images/holiday-party.png",
+  caption: "What a great holiday party omg",
+  timestamp: "11:37 PM December 31, 2019",
+  number_likes: 13,
+  comments: []
 }
